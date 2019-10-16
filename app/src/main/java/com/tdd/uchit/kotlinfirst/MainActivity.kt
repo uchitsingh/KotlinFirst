604b738e8e473a483e3743f9097bb6706e104329
uchitsingh <36070958+uchitsingh@users.noreply.github.com>
2019-10-16 18:25:17 +0100

    Adding second Actviity  -Add a button in main actviity, upon click start second actvity

--- app/src/main/java/com/tdd/uchit/kotlinfirst/MainActivity.kt
+++ app/src/main/java/com/tdd/uchit/kotlinfirst/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.tdd.uchit.kotlinfirst
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupListeners()
     }
+
+    private fun setupListeners() {
+        button.setOnClickListener { startSecondActivity() }
+    }
+
+    private fun startSecondActivity() {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
 }
